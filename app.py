dfef29750633822999455aa7f858a43ba2bba0f7
Laleh <92782353+Laleh4@users.noreply.github.com>
2022-02-01 17:04:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,14 +29,24 @@ def greet():
 #def hello(name:str):
 #    return {'message': f'Hello,{name}'}
 
+from pydantic import BaseModel
 
 
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: str
 
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+"""
 @app.post("/predict/")
 async def predictx(req: BankNote):
 
    return {'message': req}
-"""
+
 @app.post("/predict")
 async def predictx(req: BankNote):
     return {'message': req}
