f7cb24147629b6e71081289eb3f8887be3510649
Laleh <92782353+Laleh4@users.noreply.github.com>
2022-02-01 15:49:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,11 +29,25 @@ def greet():
 #@app.get("/{name}")
 #def hello(name:str):
 #    return {'message': f'Hello,{name}'}
-
+from pydantic import BaseModel
+class BankNote(BaseModel):
+    pregnacies: float
+    glucose:  float
+    bp :  float
+    skinthickness :  float
+    insulin :  float
+    bmi :  float
+    dpf:  float
+    age:  float
 
 
 
 @app.post("/predict/")
+def predictx(req: BankNote):
+
+return {'message': req}
+
+"""
 def predict(req: BankNote):
     preg=req.pregnacies
     glucose=req.glucose
@@ -44,7 +58,7 @@ def predict(req: BankNote):
     dpf=req.dpf
     age=req.age
     features=list([preg,glucose,bp,skinthickness, insulin,bmi,dpf,age])
-    """
+    
     predict=model.predict([features])
     probab=model.predict_proba([features])
     if (predict==1):
@@ -52,7 +66,6 @@ def predict(req: BankNote):
     else:
         return {"ans":"You have benn tested negative with {} probability".format(probab[0][0])}
     """
-    return {23}
     
 
     
